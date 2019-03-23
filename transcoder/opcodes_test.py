dd422d1156a9ecc0b3899e3a58c1857bab5922cd
kris <kris.kennaway@gmail.com>
2019-03-23 22:05:36 +0000

    Fix tests

--- transcoder/opcodes_test.py
+++ transcoder/opcodes_test.py
@@ -11,16 +11,35 @@ class TestOpcodes(unittest.TestCase):
         op2 = opcodes.Terminate()
         self.assertEqual(op1, op2)
 
-        op1 = opcodes.SetPage(0x20)
-        op2 = opcodes.SetPage(0x20)
+        op1 = opcodes.Nop()
+        op2 = opcodes.Nop()
         self.assertEqual(op1, op2)
 
-        op1 = opcodes.SetPage(0x20)
-        op2 = opcodes.SetPage(0x21)
+        op1 = opcodes.Ack()
+        op2 = opcodes.Ack()
+        self.assertEqual(op1, op2)
+
+        op1 = opcodes.Ack()
+        op2 = opcodes.Nop()
         self.assertNotEqual(op1, op2)
 
-        op1 = opcodes.SetPage(0x20)
-        op2 = opcodes.Terminate()
+        op1 = opcodes.TICK_OPCODES[(4, 32)](0xff, [0x01, 0x02, 0x03, 0x04])
+        op2 = opcodes.TICK_OPCODES[(4, 32)](0xff, [0x01, 0x02, 0x03, 0x04])
+        self.assertEqual(op1, op2)
+
+        # op2 has same payload but different opcode
+        op1 = opcodes.TICK_OPCODES[(4, 32)](0xff, [0x01, 0x02, 0x03, 0x04])
+        op2 = opcodes.TICK_OPCODES[(6, 32)](0xff, [0x01, 0x02, 0x03, 0x04])
+        self.assertNotEqual(op1, op2)
+
+        # op2 has different content byte
+        op1 = opcodes.TICK_OPCODES[(4, 32)](0xff, [0x01, 0x02, 0x03, 0x04])
+        op2 = opcodes.TICK_OPCODES[(4, 32)](0xfe, [0x01, 0x02, 0x03, 0x04])
+        self.assertNotEqual(op1, op2)
+
+        # op2 has different offsets
+        op1 = opcodes.TICK_OPCODES[(4, 32)](0xff, [0x01, 0x02, 0x03, 0x04])
+        op2 = opcodes.TICK_OPCODES[(4, 32)](0xff, [0x01, 0x02, 0x03, 0x05])
         self.assertNotEqual(op1, op2)
 
 
